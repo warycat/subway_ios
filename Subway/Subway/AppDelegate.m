715c0ab1207f4f6607cdce20302e41ffd0daeb9a
ludo <ludo@c3o.asia>
2013-05-13 11:29:20 +0800

    Revert "Removing Warning"
    This reverts commit 7af2dd7511ae4d404c5ef7e0909602dcd3b55ffe.

--- Subway/Subway/AppDelegate.m
+++ Subway/Subway/AppDelegate.m
@@ -23,12 +23,11 @@ BMKMapManager* _mapManager;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    _mapManager = [[BMKMapManager alloc] init];
+    _mapManager = [[BMKMapManager alloc]init];
 	BOOL ret = [_mapManager start:@"这里输入key" generalDelegate:self];
 	if (!ret) {
 		NSLog(@"manager start failed!");
 	}
-    
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     
     [TestFlight takeOff:@"9c7d0d95-9fb2-49df-9f70-601eeec1eb4b"];
